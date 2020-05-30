b09af65295a0957ca436af82afc814c836ec06e6
Issier <rjosephc3@gmail.com>
2020-05-30 17:20:34 -0400

    Hooked into PantryParserAPI and listed values from Cookbook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [recipes, setRecipes] = useState({});
+
+  useEffect(() => {
+    fetch("http://localhost:8080/recipes")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true)
+          setRecipes(result)
+        }
+      )
+  }, []);
+
+  if (!isLoaded) {
+    return <div>Loading....</div>
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Pantry Parser</h1>
+        {Object.keys(recipes).map(recipe => (
+          <div>
+            <h2> { recipe } </h2>
+            <ol>
+            {recipes[recipe]["Ingredients"].map(value => (
+                <li key={value["Name"]}>{value["Name"]}</li>
+            ))}
+            </ol>
+          </div>
+        ))}
       </header>
     </div>
   );
