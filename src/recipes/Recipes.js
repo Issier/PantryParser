07c899da2f3555a02ac3f35e87ea531cd2433b6d
Issier <rjosephc3@gmail.com>
2020-05-31 08:59:41 -0400

    Moved Recipes into its own component

---
+++ src/recipes/Recipes.js
@@ -0,0 +1,38 @@
+import React, {useState, useEffect} from 'react';
+
+export function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [recipes, setRecipes] = useState({});
+
+  useEffect(() => {
+    fetch("http://localhost:8080/recipes")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true)
+          setRecipes(result)
+        }
+      )
+  }, []);
+
+  if (!isLoaded) {
+    return <div>Loading....</div>
+  }
+
+  return (
+    <div id="recipes">
+    {Object.keys(recipes).map(recipe => (
+        <div id="mainContent">
+        <h2> { recipe } </h2>
+        <ol>
+        {recipes[recipe]["Ingredients"].map(value => (
+            <li key={value["Name"]}>{value["Name"]}</li>
+        ))}
+        </ol>
+        </div>
+    ))}
+    </div>
+  );
+}
+
+export default App;
