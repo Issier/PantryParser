e7fb3a35a61b20a85df54745edf9ae99047aa3f8
Issier <rjosephc3@gmail.com>
2020-06-13 13:51:14 -0400

    Added ingredient tab to recipe cards

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Recipes from './components/Recipes.js'; 
 import IngredientSelector from './components/IngredientSelector.js';
-import Route from 'react-router-dom/Route';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import { Redirect } from 'react-router-dom';
 
@@ -20,28 +20,26 @@ function App() {
       <header className="App-header">
         <img alt="Groceries" src={"/pantryParserLogo.png"} id="groceryImage"></img>
       </header>
-      <Route 
-        exact 
-        path="/" 
-        render={() => (
-          <div>
-            <IngredientSelector updateIngredients={updateIngredients}></IngredientSelector>
-            <Recipes selectedIngredients={selectedIngredients} updateIngredients={setSelectedIngredients}></Recipes>
-          </div>
-        )}
-      />
-      <Route 
-        exact 
-        path="/recipes/add" 
-        render={() => (
-          <div>
-            <h1>Not Implemented</h1>
-          </div>
-        )}
-      />
-      <Route path="*">
-        <Redirect to="/"></Redirect>
-      </Route>
+      <Router>
+        <Switch>
+          <Route exact path="/" render={() => (
+            <div>
+              <IngredientSelector updateIngredients={updateIngredients}></IngredientSelector>
+              <Recipes selectedIngredients={selectedIngredients} updateIngredients={setSelectedIngredients}></Recipes>
+            </div>
+          )}
+        />
+        <Route exact path="/recipes/add" render={() => (
+            <div>
+              <h1>Not Implemented</h1>
+            </div>
+          )}
+        />
+        <Route path="*">
+          <Redirect to="/"></Redirect>
+        </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,18 +1,61 @@
-import React from 'react';
-import Card from 'react-bootstrap/Card';
+import React, {useState, useEffect} from 'react';
+import {Table, Nav, Card} from 'react-bootstrap';
 import Button from 'react-bootstrap/Button'
 
 export function Recipe(props) {
+    const [key, setKey] = useState("overview");
+
+    let renderBody;
+
+    if (key === 'details') {
+        renderBody = (
+            <div>
+                <Card.Body>
+                <Card.Title>{props.recipe["name"]}</Card.Title>
+                <Card.Text>
+                    <Table>
+                        <thead>
+                            <th>Name</th>
+                            <th>Amount</th>
+                        </thead>
+                        {props.recipe["ingredients"]?.map(ingredient => (
+                            <tr>
+                                <td>{ingredient["name"]}</td>
+                                <td>?</td>
+                            </tr>
+                        ))}
+                    </Table>
+                </Card.Text>
+                </Card.Body>
+            </div>
+        )
+    } else {
+        renderBody = (
+            <div>
+                <Card.Img variant="top" src="platePlaceholder.jpg" />
+                <Card.Body>
+                <Card.Title>{props.recipe["name"]}</Card.Title>
+                <Card.Text>
+                    {props.recipe["description"]}
+                </Card.Text>
+                </Card.Body>
+            </div>
+        )
+    }
+
     return (
         <Card className="recipe" style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="platePlaceholder.jpg" />
-            <Card.Body>
-            <Card.Title>{props.recipe["name"]}</Card.Title>
-            <Card.Text>
-                {props.recipe["description"]}
-            </Card.Text>
-            <Button variant="primary">Learn More</Button>
-            </Card.Body>
+            <Card.Header>
+                <Nav defaultActiveKey="overview" fill variant="tabs" onSelect={(selectedKey) => setKey(selectedKey)}>
+                <Nav.Item>
+                    <Nav.Link eventKey="overview">Overview</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="details">Details</Nav.Link>
+                </Nav.Item>
+                </Nav>
+            </Card.Header>
+            {renderBody}
         </Card>
     )
 }
