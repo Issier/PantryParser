2767a5f572ed9d460fe64cdda12d32bb5bf7428e
Ryan Collins <rjosephc3@gmail.com>
2022-09-11 15:34:29 -0400

    Added new ingredient form

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import { Redirect } from 'react-router-dom';
 import AddRecipe from './components/AddRecipe.js';
+import AddIngredient from './components/AddIngredient.js';
 import { Navbar, Nav, NavDropdown, Toast } from 'react-bootstrap';
 
 function App() {
   const [selectedIngredients, setSelectedIngredients] = useState({})
   const [recipes, setRecipes] = useState({});
-  const [show, setShow] = useState(false);
-  const [showToast, setShowToast] = useState(false);
+  const [showAddRecipe, setShowAddRecipe] = useState(false);
+  const [showAddIngredient, setShowAddIngredient] = useState(false);
+  const [showToast, setShowToast] = useState({show: false, itemAdded: ""});
 
 
   const getRecipeList = () => {
@@ -26,26 +28,37 @@ function App() {
   }
 
   const recipeFormCloseHandler = (recipeAdded) => {
-    setShow(false);
-    setShowToast(recipeAdded);
+    setShowAddRecipe(false);
+    setShowToast({show: recipeAdded, itemAdded: "Recipe"});
+  }
+
+  const ingredientFormCloseHandler = (ingredientAdded) => {
+    setShowAddIngredient(false);
+    setShowToast({show: ingredientAdded, itemAdded: "Ingredient"});
+  }
+
+  const ItemAddedToast = () => {
+    return <Toast show={showToast.show} animation={true} delay={3000} autohide bg="success">
+              <Toast.Header closeButton={false}>
+                Success
+              </Toast.Header>
+              <Toast.Body>
+                {showToast.itemAdded} was successfully added!
+              </Toast.Body>
+            </Toast>
   }
 
   return (
     <div className="App">
-      <Toast show={showToast} animation={true} delay={3000} autohide bg="success">
-        <Toast.Header closeButton={false}>
-          Success
-        </Toast.Header>
-        <Toast.Body>
-          Recipe was successfully added!
-        </Toast.Body>
-      </Toast>
-      <AddRecipe show={show} onFormClose={recipeFormCloseHandler}/>
+      <ItemAddedToast />
+      <AddRecipe show={showAddRecipe} onFormClose={recipeFormCloseHandler}/>
+      <AddIngredient show={showAddIngredient} onFormClose={ingredientFormCloseHandler}/>
       <header className="App-header">
         <Navbar collapseOnSelect bg="light" variant="light">
           <NavDropdown id="collasible-nav-dropdown navItemsMobile">
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link onClick={() => setShow(true)}>Add Recipe</Nav.Link>
+            <Nav.Link onClick={() => setShowAddRecipe(true)}>Add Recipe</Nav.Link>
+            <Nav.Link onClick={() => setShowAddIngredient(true)}>Add Ingredient</Nav.Link>
             <Nav.Link target="_" href="https://github.com/Issier/PantryParser">Github</Nav.Link>
           </NavDropdown>
           <Navbar.Brand href="/"> 
@@ -60,7 +73,8 @@ function App() {
           </Navbar.Brand>
           <Nav>
             <Nav.Link className="navItemsDesktop" href="/">Home</Nav.Link>
-            <Nav.Link onClick={() => setShow(true)}>Add Recipe</Nav.Link>
+            <Nav.Link onClick={() => setShowAddRecipe(true)}>Add Recipe</Nav.Link>
+            <Nav.Link onClick={() => setShowAddIngredient(true)}>Add Ingredient</Nav.Link>
             <Nav.Link className="navItemsDesktop" target="_" href="https://github.com/Issier/PantryParser">Github</Nav.Link>
           </Nav>
         </Navbar>
@@ -72,10 +86,6 @@ function App() {
               <IngredientSelector getRecipeList={getRecipeList} updateIngredients={setSelectedIngredients}></IngredientSelector>
               <Recipes selectedIngredients={selectedIngredients} recipes={recipes} updateIngredients={setSelectedIngredients}></Recipes>
             </div>
-          )}
-        />
-        <Route exact path="/recipes/add" render={() => (
-            <AddRecipe></AddRecipe>
           )}
         />
         <Route path="*">
---
+++ src/components/AddIngredient.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { Form, Modal, Button } from 'react-bootstrap';
+
+const AddIngredient = (props) => {
+    const [ingredientName, setIngredientName] = useState("");
+    const [ingredientCategory, setIngredientCategory] = useState("");
+
+    const nameChangeHandler = (event) => {
+        setIngredientName(event.target.value)
+    }
+
+    const categoryChangeHandler = (event) => {
+        setIngredientCategory(event.target.value)
+    }
+
+    const submitHandler = () => {
+        fetch(`http://localhost:8080/ingredients/add`, {
+            method: 'POST',
+            body: JSON.stringify({name: ingredientName, category: ingredientCategory})
+        }).then(() => {
+            props.onFormClose(true);
+        });
+    }
+
+    const cancelHandler = () => {
+        props.onFormClose(false);
+    }
+
+    return <Modal show={props.show}>
+        <Modal.Header>
+            <Modal.Title>Add Ingredient</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <Form>
+                <Form.Group>
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control onChange={nameChangeHandler}/>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Category</Form.Label>
+                    <Form.Control onChange={categoryChangeHandler}/>
+                </Form.Group>
+            </Form>
+        </Modal.Body>
+        <Modal.Footer>
+            <Button variant="secondary" onClick={cancelHandler}>Cancel</Button>
+            <Button variant="primary" onClick={submitHandler}>Submit</Button>
+        </Modal.Footer>
+    </Modal>
+}
+
+export default AddIngredient;
