fff9d41478f09ebc00f75501da8e57c4fec7108a
Ryan Collins <rjosephc3@gmail.com>
2022-09-14 19:42:59 -0400

    Removed unused Router and Toast on form submission (will be replaced later)

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React, { useState } from 'react';
 import Recipes from './components/Recipes.js'; 
 import IngredientSelector from './components/IngredientSelector.js';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
-import { Redirect } from 'react-router-dom';
 import AddRecipe from './components/AddRecipe.js';
 import AddIngredient from './components/AddIngredient.js';
-import { Navbar, Nav, NavDropdown, Toast } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 
 function App() {
   const [selectedIngredients, setSelectedIngredients] = useState({})
   const [recipes, setRecipes] = useState({});
   const [showAddRecipe, setShowAddRecipe] = useState(false);
   const [showAddIngredient, setShowAddIngredient] = useState(false);
-  const [showToast, setShowToast] = useState({show: false, itemAdded: ""});
 
 
   const getRecipeList = () => {
@@ -29,28 +26,14 @@ function App() {
 
   const recipeFormCloseHandler = (recipeAdded) => {
     setShowAddRecipe(false);
-    setShowToast({show: recipeAdded, itemAdded: "Recipe"});
   }
 
   const ingredientFormCloseHandler = (ingredientAdded) => {
     setShowAddIngredient(false);
-    setShowToast({show: ingredientAdded, itemAdded: "Ingredient"});
-  }
-
-  const ItemAddedToast = () => {
-    return <Toast show={showToast.show} animation={true} delay={3000} autohide bg="success">
-              <Toast.Header closeButton={false}>
-                Success
-              </Toast.Header>
-              <Toast.Body>
-                {showToast.itemAdded} was successfully added!
-              </Toast.Body>
-            </Toast>
   }
 
   return (
     <div className="App">
-      <ItemAddedToast />
       <AddRecipe show={showAddRecipe} onFormClose={recipeFormCloseHandler}/>
       <AddIngredient show={showAddIngredient} onFormClose={ingredientFormCloseHandler}/>
       <header className="App-header">
@@ -79,20 +62,10 @@ function App() {
           </Nav>
         </Navbar>
       </header>
-      <Router>
-        <Switch>
-          <Route exact path="/" render={() => (
-            <div>
-              <IngredientSelector getRecipeList={getRecipeList} updateIngredients={setSelectedIngredients}></IngredientSelector>
-              <Recipes selectedIngredients={selectedIngredients} recipes={recipes} updateIngredients={setSelectedIngredients}></Recipes>
-            </div>
-          )}
-        />
-        <Route path="*">
-          <Redirect to="/"></Redirect>
-        </Route>
-        </Switch>
-      </Router>
+      <div>
+        <IngredientSelector getRecipeList={getRecipeList} updateIngredients={setSelectedIngredients}></IngredientSelector>
+        <Recipes selectedIngredients={selectedIngredients} recipes={recipes} updateIngredients={setSelectedIngredients}></Recipes>
+      </div>
     </div>
   );
 }
